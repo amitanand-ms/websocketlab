a1a96f927042f288010a78d5402e9f3af7e6975c
amitanand-ms <46723965+amitanand-ms@users.noreply.github.com>
2021-01-08 17:18:16 -0500

    Update prepareclient.sh

--- prepareclient.sh
+++ prepareclient.sh
@@ -2,5 +2,5 @@
 sudo yum install python3 -y
 sudo pip3 install asyncio
 sudo pip3 install websockets
-sudo cp ./client.py /home/testadmin/client.py
+sudo cp ./websocketlab-main/client.py /home/testadmin/client.py
 sudo chmod +x /home/testadmin/client.py
