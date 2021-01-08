f796721dde00183407a84224ef4bee6be6bbf972
amitanand-ms <46723965+amitanand-ms@users.noreply.github.com>
2021-01-08 16:19:45 -0500

    Update prepareserver.sh

--- prepareserver.sh
+++ prepareserver.sh
@@ -3,6 +3,6 @@ sudo yum install python3 -y
 sudo pip3 install asyncio
 sudo pip3 install websockets
 sudo echo "10.1.1.10 localserver" >> /etc/hosts
-sudo chmod +x websocket_server.py
+sudo chmod +x ./websocket_server.py
 sudo ./websocket_server.py &
 
